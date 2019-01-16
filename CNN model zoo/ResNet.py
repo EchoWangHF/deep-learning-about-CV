2c9c17d4e33fce3fe0064a03c972cb4cf691e799
Wang Jun <1427014893@qq.com>
2019-01-16 14:42:08 +0800

    Create ResNet.py

---
+++ CNN model zoo/ResNet.py
@@ -0,0 +1,94 @@
+import gluonbook as gb
+import mxnet as mx
+from mxnet import autograd,gluon,init,nd
+from mxnet.gluon import loss as gloss,nn
+import time
+
+# ResNet相关部分
+class Residual(nn.Block):
+    def __init__(self,num_channels,use_1x1conv=False,strides=1,**kwargs):
+        super(Residual, self).__init__(**kwargs)
+        self.conv1=nn.Conv2D(num_channels,kernel_size=3,padding=1,strides=strides)
+        self.conv2=nn.Conv2D(num_channels,kernel_size=3,padding=1)
+        if use_1x1conv:
+            self.conv3=nn.Conv2D(num_channels,kernel_size=1,strides=strides)
+        else:
+            self.conv3=None
+        self.bn1=nn.BatchNorm()
+        self.bn2=nn.BatchNorm()
+
+    def forward(self,X):
+        Y=nd.relu(self.bn1(self.conv1(X)))
+        Y=self.bn2(self.conv2(Y))
+        if self.conv3:
+            X=self.conv3(X)
+        return nd.relu(Y+X)
+
+def resnet_block(num_channels, num_residuals, first_block=False):
+    blk = nn.Sequential()
+    for i in range(num_residuals):
+        if i == 0 and not first_block:
+            blk.add(Residual(num_channels, use_1x1conv=True, strides=2))
+        else:
+            blk.add(Residual(num_channels))
+    return blk
+    
+def ResNet(outchannels):
+    net=nn.Sequential()
+    net.add(
+        nn.Conv2D(64,kernel_size=7,strides=2,padding=3),
+        nn.BatchNorm(),
+        nn.Activation('relu'),
+        nn.MaxPool2D(pool_size=3,strides=2,padding=1),
+        resnet_block(64, 2, first_block=True),
+        resnet_block(128, 2),
+        resnet_block(256, 2),
+        resnet_block(512, 2),
+        nn.GlobalAvgPool2D(), 
+        nn.Dense(outchannels)
+    )
+    return net
+
+
+def try_gpu4():
+    try:
+        ctx=mx.gpu()
+        _=nd.zeros((1,),ctx=ctx)
+    except mx.base.MXNetError:
+        ctx=mx.cpu()
+    return ctx
+
+def evaluate_accuracy(data_iter,net,ctx):
+    acc=nd.array([0],ctx=ctx)
+    for X,y in data_iter:
+        X,y=X.as_in_context(ctx),y.as_in_context(ctx)
+        acc+=gb.accuracy(net(X),y)
+    return acc.asscalar()/len(data_iter)
+
+def train_module(net,train_iter,test_iter,batch_size,trainer,ctx,num_epochs):
+    print('training on',ctx)
+    loss=gloss.SoftmaxCrossEntropyLoss()
+    for epoch in range(num_epochs):
+        train_l_sum,train_acc_sum,start=0,0,time.time()
+        for X,y in train_iter:
+            X,y=X.as_in_context(ctx),y.as_in_context(ctx)
+            with autograd.record():
+                y_hat=net(X)
+                l=loss(y_hat,y)
+            l.backward()
+            trainer.step(batch_size)
+            train_l_sum+=l.mean().asscalar()
+            train_acc_sum+=gb.accuracy(y_hat,y)
+        test_acc=evaluate_accuracy(test_iter,net,ctx)
+        print('epoch %d,loss%.4f,train acc %.3f,test acc %.3f,time %.1f sec'%(epoch+1,train_l_sum/len(train_iter),
+              train_acc_sum/len(train_iter),test_acc,time.time()-start))
+
+batch_size = 256
+train_iter, test_iter = gb.load_data_fashion_mnist(batch_size=batch_size,resize=96)
+lr, num_epochs = 0.05, 5
+net=ResNet(10)
+ctx=try_gpu4()
+net.initialize(force_reinit=True, ctx=ctx, init=init.Xavier())
+trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': lr})
+train_module(net, train_iter, test_iter, batch_size, trainer, ctx, num_epochs)
+
